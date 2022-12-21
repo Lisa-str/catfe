57f8f806c9a320169f56efe446aeb5928b72ddf6
Lisa-str <Lisa.Staruch@studentfr.ch>
2022-12-21 10:55:44 +0100

    js boucle

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,14 @@
 var burgerMenu = document.querySelector(".burger-menu");
 var Nav = document.querySelector("nav");
-var Link1 = document.querySelector(".link1");
-var Link2 = document.querySelector(".link2");
-var Link3 = document.querySelector(".link3");
-var Link4 = document.querySelector(".link4");
-
-console.log(Nav, burgerMenu);
 
 burgerMenu.addEventListener("click", () => {
   Nav.classList.toggle("menu-open");
 });
 
-Link1.addEventListener("click", () => {
-  Nav.classList.remove("menu-open");
-});
+var links = document.querySelectorAll(".link");
 
-Link2.addEventListener("click", () => {
-  Nav.classList.remove("menu-open");
-});
-
-Link3.addEventListener("click", () => {
-  Nav.classList.remove("menu-open");
-});
-
-Link4.addEventListener("click", () => {
-  Nav.classList.remove("menu-open");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    Nav.classList.remove("menu-open");
+  });
 });
